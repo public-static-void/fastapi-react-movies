20fc208258ed9b2d9590345a3748203f86c79760
Vadim Titov <vadim.titov.hh@gmail.com>
2024-10-29 19:58:08 +0100

    fix: msw update broke jest

--- frontend/movies_frontend/jest.setup.ts
+++ frontend/movies_frontend/jest.setup.ts
@@ -1,3 +1,9 @@
+import { BroadcastChannel } from 'worker_threads';
+(global as any).BroadcastChannel = BroadcastChannel;
+
+import { TransformStream } from 'node:stream/web';
+global.TransformStream = TransformStream as typeof globalThis.TransformStream;
+
 import '@testing-library/jest-dom';
 import { server } from './src/mocks/server.js';
 
