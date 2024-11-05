79ab3c919b3b7817b3039a822e712e39704a7463
Vadim Titov <vadim.titov.hh@gmail.com>
2024-11-05 15:18:46 +0100

    fix: remove hotfix

--- frontend/movies_frontend/jest.setup.ts
+++ frontend/movies_frontend/jest.setup.ts
@@ -1,10 +1,3 @@
-import { BroadcastChannel } from 'worker_threads';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-(global as any).BroadcastChannel = BroadcastChannel;
-
-import { TransformStream } from 'node:stream/web';
-global.TransformStream = TransformStream as typeof globalThis.TransformStream;
-
 import '@testing-library/jest-dom';
 import { server } from './src/mocks/server.js';
 
